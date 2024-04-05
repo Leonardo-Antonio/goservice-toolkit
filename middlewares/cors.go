55e493e6a55a111b625551031d873c327a0e7726
Leonardo Nolasco <leonardo.nolasco@gdpteam.com>
2024-04-04 22:46:37 -0500

    [feat] add all methods and headers all

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -5,8 +5,8 @@ import "net/http"
 func CorsHeader(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 
 		// Si es una solicitud OPTIONS, respondemos con un status OK
 		if r.Method == "OPTIONS" {
