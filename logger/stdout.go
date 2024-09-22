3c590897da7000e6111e01516a2e2f9adf0782f9
Leonardo Nolasco <leonardo.nolasco@gdpteam.com>
2024-09-21 21:23:07 -0500

    feat: log v1

---
+++ logger/stdout.go
@@ -0,0 +1,58 @@
+package logger
+
+import (
+	"os"
+
+	logOrigin "log"
+
+	"github.com/sirupsen/logrus"
+)
+
+var log *logrus.Logger
+
+func init() {
+	log = logrus.New()
+	log.Formatter = &logrus.JSONFormatter{
+		PrettyPrint: true,
+		DataKey:     "data",
+	}
+	log.Out = os.Stdout
+	logOrigin.Println("✅ Log configured")
+}
+
+type (
+	level string
+	Input struct {
+		Message string         `json:"message"`
+		Data    map[string]any `json:"data"`
+		Level   level          `json:"type"`
+	}
+)
+
+const (
+	Debug level = "debug"
+	Info  level = "info"
+	Warn  level = "warn"
+	Error level = "error"
+)
+
+func Write(in Input) {
+	var level logrus.Level
+	switch in.Level {
+	case Debug:
+		level = logrus.DebugLevel
+	case Info:
+		level = logrus.InfoLevel
+	case Warn:
+		level = logrus.WarnLevel
+	case Error:
+		level = logrus.ErrorLevel
+	}
+
+	if in.Data == nil {
+		log.WithFields(nil).Log(level, in.Message)
+		return
+	}
+
+	log.WithFields(in.Data).Log(level, in.Message)
+}
